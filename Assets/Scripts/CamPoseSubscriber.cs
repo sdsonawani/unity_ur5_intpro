8603c66fddaf82bdc99c973246db837db050e915
Shubham Sonawani <sdsonawa@asu.edu>
2022-12-19 16:22:47 -0700

    updated version with somewhat working 3D effect

--- Assets/Scripts/CamPoseSubscriber.cs
+++ Assets/Scripts/CamPoseSubscriber.cs
@@ -15,6 +15,7 @@ using System;
 public class CamPoseSubscriber: MonoBehaviour{
 
     public GameObject Cylinder;
+    public GameObject Cube;
     public Camera POV = new Camera();
     ROSConnection ros;
     public string topicName = "pov_pose";
@@ -34,7 +35,10 @@ public class CamPoseSubscriber: MonoBehaviour{
         Vector3 c_trans = new Vector3(0.027f,0.65f,0.384f);
         Quaternion c_quat = Quaternion.Euler(0,0,0);
         Cylinder.transform.SetPositionAndRotation(c_trans,c_quat);
-        // POV.transform.SetParent(Cylinder.transform);
+        Vector3 cube_trans = new Vector3(0.027f ,0.65f +0.15f,0.384f);
+        Quaternion cube_quat = Quaternion.Euler(0,0,0);
+        Cube.transform.SetPositionAndRotation(cube_trans,cube_quat);
+        // POV.transform.SetParent(Cube.transform);
         // cam  = Instantiate(POV);
         POV.enabled = true;
         // POV.tag = "POV_camera"
@@ -80,7 +84,30 @@ public class CamPoseSubscriber: MonoBehaviour{
         // POV.transform.Translate(0,offset,0);
         // Vector3 trans = new Vector3(0,1,1);
         // Vector3 trans = new Vector3(trans_x+0.1f,trans_y+0.1f,trans_z+0.1f);
-        Vector3 trans = new Vector3(trans_x,trans_y + 0.5f,trans_z);
+
+        Vector3 trans = new Vector3(trans_x,trans_y + 1.0f,trans_z);
+        Vector3 trans1 = new Vector3(trans_x,trans_y,trans_z);
+        Vector3 cube_trans = new Vector3(0.027f ,0.65f +0.15f,0.384f);
+        Vector3 relative_pose = cube_trans - trans;
+
+
+        
+        var rad   = relative_pose.magnitude;
+        var theta = Math.Atan(relative_pose[1]/relative_pose[0]);
+        var phi   = Math.Acos(relative_pose[2]/rad);
+        Debug.Log(rad);
+
+        var rad2  = 1.5f;
+        var x_    = rad2 * Math.Cos(theta) * Math.Sin(phi);
+        var y_    = rad2 * Math.Sin(theta) * Math.Sin(phi);
+        var z_    = rad2 * Math.Cos(phi);
+        var new_xyz = new Vector3((float)x_,(float)y_, (float)z_);
+        var new_pose = new_xyz - cube_trans;
+
+
+        // Vector3 relative_pose = trans - cube_trans;
+        Quaternion rotation = Quaternion.LookRotation(relative_pose);
+        // Vector3 trans = new Vector3(trans_x,trans_y,trans_z);
         // Quaternion quat  = new Quaternion(0,0,0,1);
         Quaternion quat  = new Quaternion(quat_x,quat_y,quat_z,quat_w);
         // Quaternion quat  = Quaternion.Euler(135,0,180);
@@ -90,6 +117,14 @@ public class CamPoseSubscriber: MonoBehaviour{
         // POV.transform.SetLocalPositionAndRotation(trans,quat);
 
         POV.transform.SetPositionAndRotation(trans,quat);
+        // POV.transform.SetPositionAndRotation(trans,rotation);
+        // POV.transform.SetPositionAndRotation(new_pose,quat);
+
+        Vector3 lookat_axis = new Vector3(0,1,0);
+        // Vector3 lookat_axis = new Vector3(0.5f,0.5f,0.5f);
+        POV.transform.LookAt(Cube.transform,lookat_axis);
+        // POV.transform.LookAt(Cube.transform, Vector3.up);
+        // POV.transform.LookAt(Cylinder.transform);
         // POV.transform.position(0,1,0);
         // offset = offset - 0.001f;
         // Debug.Log(string.Format("translation: [{0},{1},{2}]",trans_x,trans_y,trans_z));    
