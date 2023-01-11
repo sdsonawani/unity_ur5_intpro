bdd4f027a3085904223b768804de8e4b23c0ea30
Shubham Sonawani <sdsonawa@asu.edu>
2023-01-10 19:28:12 -0700

    shadow and real with time delta

--- Assets/Scripts/SubscribeUr5Joints.cs
+++ Assets/Scripts/SubscribeUr5Joints.cs
@@ -18,7 +18,6 @@ public enum ControlType { PositionControl };
 public class SubscribeUr5Joints : MonoBehaviour
 {
     public GameObject ur5;
-    public GameObject ur5_delta;
     public float publisheFreq = 30f;
     public float deltaTime = 0.1f;
     private float publishMessageTime;
@@ -34,7 +33,6 @@ public class SubscribeUr5Joints : MonoBehaviour
                                                 "/wrist_2_link",  
                                                 "/wrist_3_link" };
     ArticulationBody[] m_ur5ArticulationBody;
-    ArticulationBody[] m_ur5ArticulationBody_delta;
 
 
 
@@ -54,7 +52,6 @@ public class SubscribeUr5Joints : MonoBehaviour
         publishMessageTime = (1/publisheFreq);
         // find specific game object
         ur5 = GameObject.Find("ur5");
-        ur5_delta = GameObject.Find("ur5_1");
         // ros subscriber 
         // ROSConnection.GetOrCreateInstance().Subscribe<Ur5JointsMsg>("real_ur5/joints", jointCallback);
         ROSConnection.GetOrCreateInstance().Subscribe<Ur5JointsMsg>("ur5_goal/joints", jointCallback);
@@ -62,11 +59,9 @@ public class SubscribeUr5Joints : MonoBehaviour
         // load articulate body with non static joints
         var link_names              = string.Empty;
         m_ur5ArticulationBody       = new ArticulationBody[num_joints];
-        m_ur5ArticulationBody_delta = new ArticulationBody[num_joints];
         for (var i = 0; i < num_joints; i++){
             link_names += LinkNames[i];
             m_ur5ArticulationBody[i]       = ur5.transform.Find(link_names).GetComponent<ArticulationBody>();
-            m_ur5ArticulationBody_delta[i] = ur5_delta.transform.Find(link_names).GetComponent<ArticulationBody>();
             // Debug.Log(string.Format("Articulate body: {0} and joint position: {1}",m_ur5ArticulationBody[i],m_ur5ArticulationBody[i].jointPosition[0]));
         }
 
@@ -141,39 +136,14 @@ public class SubscribeUr5Joints : MonoBehaviour
             // Debug.Log(string.Format("goal joints: [{0},{1},{2},{3},{4},{5}]",joints_g_debug[0], joints_g_debug[1], joints_g_debug[2], joints_g_debug[3], joints_g_debug[4], joints_g_debug[5]));            
     }
     
-
-    IEnumerator waiterTest(){
-
-        timeElapsed1 += Time.deltaTime;
-        if (timeElapsed1 > publishMessageTime){ 
-            applyControl(m_ur5ArticulationBody);
-            timeElapsed1 = 0;
-        }
-
-        yield return new WaitForSeconds(deltaTime);
-        
-
-        timeElapsed2 += Time.deltaTime;
-        if (timeElapsed2 > publishMessageTime){ 
-            applyControl(m_ur5ArticulationBody_delta);
-            timeElapsed2 = 0;
-        }
-
-    }
-
-
     void Update(){
         
         timeElapsed1 += Time.deltaTime;
         publishMessageTime = (1/publisheFreq);
         if (timeElapsed1 > publishMessageTime)
         {   
-            
             applyControl(m_ur5ArticulationBody);
-            applyControl(m_ur5ArticulationBody_delta);
-
             timeElapsed1 = 0;
-            
         }
     }
 }
