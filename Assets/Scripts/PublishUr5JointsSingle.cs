24a6ea7e219ce16637262c8a4e4c48fe2fb4215a
Fabian Weigend <fweigend@asu.edu>
2023-01-21 16:00:08 -0700

    Create simple stream scene with a single robot for smartwatch debugging

---
+++ Assets/Scripts/PublishUr5JointsSingle.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RosMessageTypes.Ur5Intpro;
+using Unity.Robotics.ROSTCPConnector;
+using Unity.Robotics.UrdfImporter;
+using System;
+
+public class PublishUr5JointsSingle : MonoBehaviour
+{
+    ROSConnection ros;
+    public string topicName = "unity_ur5/joints";
+    const int num_joints = 6;
+
+
+    // The game object
+    public GameObject ur5;
+    // Publish the cube's position and rotation every N seconds
+    public float publishMessageFrequency = 0.01f;
+    // Used to determine how much time has elapsed since the last message was published
+    private float timeElapsed;
+
+    public static readonly string[] LinkNames =
+        { "world/dummy_link/base_link/shoulder_link", "/upper_arm_link", "/forearm_link", "/wrist_1_link",  "/wrist_2_link",  "/wrist_3_link" };
+
+    UrdfJointRevolute[] m_jab;
+    UrdfJointRevolute[] m_Shadow_jab;
+
+    void Start()
+    {
+        // start the ROS connection
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<Ur5JointsMsg>(topicName);
+        // Debug.developerConsoleVisible = true;
+        
+        m_jab = new UrdfJointRevolute[num_joints];
+        var link_names = string.Empty;
+        for (var i = 0; i < num_joints; i++){
+            link_names += LinkNames[i];
+            m_jab[i] = ur5.transform.Find(link_names).GetComponent<UrdfJointRevolute>();
+            // Debug.Log(string.Format("{0}",link_names));
+        }
+    }
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > publishMessageFrequency)
+        {      
+            // double[] j_ = new double[] {0.1,0.1,0.1,0.1,0.1,0.1};
+            double[] j_ = new double[6];
+            for(var i = 0; i<num_joints; i++ ){
+
+                if (i==1 || i == 3){
+                   j_[i] =  m_jab[i].GetPosition() - (Math.PI/2.0);
+                }
+                else{
+                    j_[i] = m_jab[i].GetPosition();
+                }
+                // Debug.Log(string.Format("{0}",j_[i]));
+            }
+            Ur5JointsMsg joints = new Ur5JointsMsg(j_);
+
+
+            ros.Publish(topicName, joints);
+            timeElapsed = 0;
+        }
+    }
+}
